4f302ef38ce86a0d2e6703b59173159208bbd031
JanisChiffelle <janischiffelle@hotmail.ch>
2023-07-10 15:06:58 +0200

    clean and add tests

--- tests.py
+++ tests.py
@@ -1,33 +1,11 @@
 import pytest
-import os
+import gcsfs
 
-from google.cloud import storage
-from main import create_blob, create_bucket
+class TestConnectivity:
+    fs_gcs = gcsfs.GCSFileSystem(project='prediswiss')
+    bucket_name = "prediswiss-parquet-data-daily"
+    bucket_data = "prediswiss-parquet-data"
 
-class TestBucket:
-    bucket_name = "prediswiss_test_bucket"
-    blob_name = "test"
-    blob_type = "text/xml"
-    blob_data = "<test>test</test>"
-    storage_client = storage.Client(project="prediswiss")
-
-    @pytest.fixture(autouse=True)
-    def run_around_tests(self):
-        yield
-        try:
-            bucket = self.storage_client.get_bucket(self.bucket_name)
-            bucket.delete(force=True)
-        except:
-            print("ok : no bucket for test")
-
-    def test_create_bucket(self):
-        create_bucket(self.bucket_name, self.storage_client)
-        try:
-            self.storage_client.get_bucket(self.bucket_name)
-        except Exception:
-            assert False
-
-    def test_create_blob(self):
-        bucket = create_bucket(self.bucket_name, self.storage_client)
-        create_blob(bucket, self.blob_name, self.blob_type, self.blob_data)
-        assert bucket.get_blob(self.blob_name).download_as_text() == self.blob_data
+    def test_bucket_data_exist(self):
+        assert self.fs_gcs.exists(self.bucket_data)
+        assert self.fs_gcs.exists(self.bucket_name)
